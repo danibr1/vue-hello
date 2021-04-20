0def6b0d071a09c2be43f753ff49b8609d486f28
danibr1 <75913820+danibr1@users.noreply.github.com>
2021-04-20 22:17:13 +0200

    completed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 const app = new Vue({
     el: '#app',
     data: {
-        title: 'The biggest ICO of 2021!',
+        title: 'STUDENT COIN ',
+        subTitle: 'The biggest ICO of 2021!',
         image: './img/stc.png',
     }
 });
